eb1f75f4f07d7a06abadc074ade7a9d3002b47ad
Aronk <furretkachu1017@gmail.com>
2026-04-13 14:23:10 +0200

    Create procedure to filter reviews by rating
    Added a stored procedure to filter reviews based on content ID and rating range.

--- programacion_proyecto.sql
+++ programacion_proyecto.sql
@@ -1,1 +1,12 @@
-
+USE streamingdb;
+-- ----------------------------------------------------------
+-- Un procedimiento de consulta con parámetros de filtrado
+-- ----------------------------------------------------------
+DELIMITER //
+DROP PROCEDURE IF EXISTS filtrar_resenias;
+CREATE PROCEDURE filtrar_resenias(IN id_cont INT, IN min_puntuacion TINYINT, IN max_puntuacion TINYINT)
+BEGIN
+	SELECT * FROM reseña WHERE id_contenido = id_cont && puntuacion >= min_puntuacion && puntuacion <= max_puntuacion;
+END//
+DELIMITER ;
+CALL filtrar_resenias(100, 8, 10);
